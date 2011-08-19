5a71bee8e52988de2ff6c80a12edd8088be7dbbb
Charles Wright <charles.wright@gmail.com>
2011-08-19 12:24:26 -0700

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User
+  include MongoMapper::Document
+  MongoMapper.database = 'testappdatabase'
+  
+  key :name, String, :required => true, :unique => true
+  timestamps!
+  #many :drinks
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testapp::Application.config.secret_token = 'e54e6d56a6ccc167b3e1ce2a68af93dfce63818333dba2c1a54ccb4450205d797d8b90cf281d6a630fdd21acf06832ae399300b971d9f4d38f47b85f32176029'
